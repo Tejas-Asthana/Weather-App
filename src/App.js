d09f1497c67cc42462e5eb584b37ab1c28388136
tejas <tejasa82@gmail.com>
2020-08-10 09:55:30 +0530

    third commit. Replace Fontawesome icons with svgs

--- src/App.js
+++ src/App.js
@@ -87,10 +87,26 @@ class App extends React.Component {
       <div className="App">
         <div className="search-bar">
           <div className="search-header">
-            <i className="fas fa-search-location" />
+            <span className="location-icon">
+              <svg viewBox="0 0 576 512" width="100" title="map-marked-alt">
+                <path d="M288 0c-69.59 0-126 56.41-126 126 0 56.26 82.35 158.8 113.9 196.02 6.39 7.54 17.82 7.54 24.2 0C331.65 284.8 414 182.26 414 126 414 56.41 357.59 0 288 0zm0 168c-23.2 0-42-18.8-42-42s18.8-42 42-42 42 18.8 42 42-18.8 42-42 42zM20.12 215.95A32.006 32.006 0 0 0 0 245.66v250.32c0 11.32 11.43 19.06 21.94 14.86L160 448V214.92c-8.84-15.98-16.07-31.54-21.25-46.42L20.12 215.95zM288 359.67c-14.07 0-27.38-6.18-36.51-16.96-19.66-23.2-40.57-49.62-59.49-76.72v182l192 64V266c-18.92 27.09-39.82 53.52-59.49 76.72-9.13 10.77-22.44 16.95-36.51 16.95zm266.06-198.51L416 224v288l139.88-55.95A31.996 31.996 0 0 0 576 426.34V176.02c0-11.32-11.43-19.06-21.94-14.86z" />
+              </svg>
+            </span>
             <input type="type" onKeyPress={(e) => {if(e.key === 'Enter' && this.state.inputCheck === true) this.search()}} placeholder="Type city name here" value={this.state.cityName} onChange={e => this.handleChange(e)} />
-            <button onClick={this.clearSearch} disabled={this.state.cityName.length>0 ? false : true} ><i style={{color: this.state.cityName.length > 0 ? "#ff5959" : "grey" }} className="fas fa-times" /></button>
-            <button onClick={this.setSearch} disabled={this.state.inputCheck ? false : true} ><i style={{color: this.state.inputCheck ? "#7f81d6" : "grey" }} className="fas fa-angle-double-right" /></button>
+            <button onClick={this.clearSearch} disabled={this.state.cityName.length>0 ? false : true} >
+            <span className="close-icon">
+              <svg style={{fill: this.state.cityName.length > 0 ? "#ff5959" : "grey" }} viewBox="0 0 352 512" width="100" title="times">
+                <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+              </svg>
+            </span>
+            </button>
+            <button onClick={this.setSearch} disabled={this.state.inputCheck ? false : true} >
+              <span className="go-search-icon">
+                <svg style={{fill: this.state.inputCheck ? "#7f81d6" : "grey" }} viewBox="0 0 448 512" width="100" title="angle-double-right">
+                  <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z" />
+                </svg>
+              </span>
+            </button>
           </div>
           {this.state.suggestions[0] === this.state.cityName ? this.setState(prev => {return {search: prev.cityName, suggestions: [], inputCheck: true}}) : this.renderSuggestions() }
         </div>
